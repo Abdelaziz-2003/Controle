a60ef33afbf01097f8a03cf2f44348f43e0fb97e
abdelaziz <Merzouk.Abdelaziz@emsi-edu.ma>
2024-05-17 23:48:17 +0100

    Initial commit

---
+++ src/main/java/ma/formations/jpa/service/IService.java
@@ -0,0 +1,7 @@
+package ma.formations.jpa.service;
+import java.util.List;
+import ma.formations.jpa.service.model.Article;
+public interface IService {
+Boolean checkAccount(String username,String password);
+List<Article> getAllArticle();
+}
